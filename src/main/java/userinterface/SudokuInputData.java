ff2f04d95c3b7d7a18bce7cc60c2211847971dc8
admin <atishay.jain.official1@gmail.com>
2025-06-22 02:44:27 +0530

    Modifying UserInputData class to SudokuInputData class

--- src/main/java/userinterface/SudokuInputData.java
+++ src/main/java/userinterface/SudokuInputData.java
@@ -2,7 +2,13 @@ package userinterface;
 
 import interfaces.UserInput;
 
-public record UserInputData(int row, int col, int value) implements UserInput {
+public record SudokuInputData(int row, int col, int value) implements UserInput {
+    public SudokuInputData(int row, int col, int value) {
+        this.row = Math.max(row, 0);
+        this.col = Math.max(col, 0);
+        this.value = Math.max(value, 0);
+    }
+
     @Override
     public int getRow() {
         return this.row;
