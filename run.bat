ea502ea6af5165384d9bb9fb0beea000cdd971d5
admin <atishay.jain.official1@gmail.com>
2025-06-21 23:41:19 +0530

    Adding libs directory to the class path

--- run.bat
+++ run.bat
@@ -1,10 +1,11 @@
-@ECHO off
+:: @ECHO off
 
 :: ---Initialization ---
 setlocal
 echo Checking requirements...
-SET "SRC_PATH=.\src\main\java\"
+SET "SRC_PATH=.\src\main\java"
 SET "OUT_PATH=.\out\production\sudoku"
+SET "LIB_PATH=.\libs"
 if not exist %SRC_PATH% (
 	ECHO Batch file has been moved or the source folder does not exits!!
 	endlocal
@@ -16,13 +17,14 @@ if not exist %OUT_PATH% (
 
 :: ---Main Execution ---
 echo Compiling the code...
-javac -d %OUT_PATH% -sourcepath %SRC_PATH% ^
-	%SRC_PATH%/*.java
+dir /s /b %SRC_PATH%\*.java > temp
+javac -cp LIB_PATH\*.jar -d %OUT_PATH% @temp
 echo Running the code...
-java -cp %OUT_PATH% SudokuApp
+java -cp %OUT_PATH%;%LIB_PATH%\*.jar SudokuApp
 
 :: ---Exit Script ---
 echo Cleaning up...
+del temp
 rd /S /Q out
 endlocal
 exit /b 0
